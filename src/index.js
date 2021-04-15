be82fd036c2ca91ebd577fcf3551982d17e62c77
willperez23 <william.perez.23@dartmouth.edu>
2021-04-15 15:04:10 -0400

    dist folder files :))

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('starting up!');
+// change require to es6 import style
+import $ from 'jquery';
+import './style.scss';
+
+let num = 0;
+
+setInterval(() => { $('#main').html(`Youve been on this page for ${num} seconds.`); num += 1; }, 1000);
+// const $ = require('jquery');
