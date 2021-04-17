ebc8809693ff0c48ce1cdb5b1cb8c2039fde3245
willperez23 <william.perez.23@dartmouth.edu>
2021-04-17 12:12:57 -0400

    react updates

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 // change require to es6 import style
-import $ from 'jquery';
+// import $ from 'jquery';
+// import './style.scss';
+
+// let num = 0;
+
+// setInterval(() => { $('#main').html(`Youve been on this page for ${num} seconds.`); num += 1; }, 1000);
+// // const $ = require('jquery');
+
+import React from 'react';
+import ReactDOM from 'react-dom';
 import './style.scss';
 
-let num = 0;
+const App = () => <div className="test">All the REACT are belong to us!</div>;
 
-setInterval(() => { $('#main').html(`Youve been on this page for ${num} seconds.`); num += 1; }, 1000);
-// const $ = require('jquery');
+ReactDOM.render(<App />, document.getElementById('main'));
